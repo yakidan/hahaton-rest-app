f49559c3ece5315f34ebf06fb9b782b3e3f87eca
Daniil Yakimov <8spider-man@mail.ru>
2022-10-01 19:20:37 +0300

    changes 11

--- src/main/java/com/yakidan/spring/spring_boot/repository/UserRepository.java
+++ src/main/java/com/yakidan/spring/spring_boot/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.yakidan.spring.spring_boot.repository;
 
 import com.yakidan.spring.spring_boot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -10,5 +11,7 @@ import javax.transaction.Transactional;
 @Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Query("select u from User u where u.nickname=?1")
+    public User findByNickname(String nickname);
 
 }
