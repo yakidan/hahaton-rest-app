3af5e95c0394489324be0cb3842c319dc0ed3b9f
Daniil Yakimov <8spider-man@mail.ru>
2022-10-01 18:22:55 +0300

    changes 9

--- src/main/java/com/yakidan/spring/spring_boot/entity/User.java
+++ src/main/java/com/yakidan/spring/spring_boot/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private int id;
 
